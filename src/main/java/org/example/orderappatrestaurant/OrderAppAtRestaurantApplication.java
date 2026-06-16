f35e6d168ebc2ca69bacb055a0f9f9123944ccec
kosei <kouseeeei2525@gmail.com>
2026-06-16 22:10:06 +0900

    initialize project.

---
+++ src/main/java/org/example/orderappatrestaurant/OrderAppAtRestaurantApplication.java
@@ -0,0 +1,13 @@
+package org.example.orderappatrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderAppAtRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderAppAtRestaurantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/orderappatrestaurant/OrderAppAtRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.orderappatrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderAppAtRestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
